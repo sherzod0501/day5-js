f3497a2e149df330ad8405a9734d842e7e5bbdf5
sher0501 <asoggamer@gmail.com>
2022-04-03 16:19:20 +0500

    day5-js

---
+++ Js/index.js
@@ -0,0 +1,14 @@
+ let one = Math.round(Math.random()*100);
+ console.log(one);
+
+ if (one > 1 && one <= 18)
+ console.log('Maktab yoshi');
+
+ else if (one > 18 && one <= 24)
+ console.log('Universtitet yoshi');
+
+ else if (one > 24 && one <= 40)
+ console.log('Ishlas yoshi');
+
+ else if (one > 40 && one <= 100)
+ console.log('Nuroniylar yoshi');
